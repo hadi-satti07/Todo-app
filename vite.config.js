08163188e70381fe23da200c162ef0b8a75724a4
hadi-satti07 <abdulsatti067@gmail.com>
2026-03-23 22:03:18 +0500

    fix configure output dir for vercel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build'  // default 'dist' ko 'build' me change kar diya
+    outDir: 'dist'  // default 'dist' ko 'build' me change kar diya
   }
 })
